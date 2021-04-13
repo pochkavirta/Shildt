72448911702bf655e66526b986907c4c67dd28b8
PochkaVirta <cyberdog771@gmail.com>
2021-04-13 22:11:26 +0300

    добавил программу

---
+++ src/Sound.java
@@ -0,0 +1,8 @@
+public class Sound {
+    public static void main(String[] args) {
+        double echo, dist;
+        echo = 1100 * 7.2;
+        dist = echo / 2;
+        System.out.println("Расстояние до скалы " + dist + " футов");
+    }
+}
